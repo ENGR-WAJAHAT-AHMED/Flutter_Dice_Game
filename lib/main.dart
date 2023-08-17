bed8d4a3246e8c051769b10014b06ce25af38fea
ENGR-WAJAHAT-AHMED <wajahatahmed621@gmail.com>
2023-08-17 21:25:20 +0500

    add complete Dice Game

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.green,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+
+
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+
+  void dicenumber(){
+    setState(() {
+      rightdicenumber = Random().nextInt(6)+1;
+      leftdicenumber = Random().nextInt(6)+1;
+
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                 dicenumber();
+                },
+                child: Image.asset('images/dice$leftdicenumber.png')
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  dicenumber();
+                },
+                child: Image.asset('images/dice$rightdicenumber.png')
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
